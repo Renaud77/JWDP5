003b3e19f28c03c79b35385fd329fdbce9555b4b
Renaud <renaud_lefebvre@yahoo.fr>
2021-07-28 15:49:29 +0100

    ajout de la page  contact

--- front-end-bootsraps/js/description.js
+++ front-end-bootsraps/js/description.js
@@ -15,18 +15,18 @@ const showCameraProduct = async (camera) => {
   const cameraProduct = document.querySelector("#product");
 
   cameraProduct.innerHTML = `
-  <div class="card mx-auto m-3" style="width: 25rem;" >
+  <div class="card mx-auto m-4" style="width: 25rem;" >
   <img class="card-img-top" src="${camera.imageUrl}">
 <div class="card-body d-flex flex-column ">
-    <h3 class="card-title">${camera.name}</h3>
-    <span>${camera.description}</span>
-    <span>${camera.price / 100}€</span>
+    <h3 class="card-title mb-3">${camera.name}</h3>
+    <span class="fst-italic mb-2">${camera.description}</span>
+    <span class="fw-bold text-primary">${camera.price / 100}€</span>
     <label for="lenses-select">choisissez votre objectif:</label>
     <select id="lenses">
     <option value="">--Selectionnez votre objetif ici--</option>
     ${camera.lenses.map((lense) => `<option>${lense}</option>`)}
     </select>
-    <button href="#" class="btn btn-warning">Commander</button>
+    <button href="#" class="btn btn-warning mt-5">Ajouter au panier </button>
 </div>
       `;
 };
