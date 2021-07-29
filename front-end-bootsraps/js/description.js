aef688aeabd28fb38a8771ab6ce5a79a540d24b9
Renaud <renaud_lefebvre@yahoo.fr>
2021-07-29 17:02:54 +0100

    RAS

--- front-end-bootsraps/js/description.js
+++ front-end-bootsraps/js/description.js
@@ -16,21 +16,28 @@ const showCameraProduct = async (camera) => {
 
   cameraProduct.innerHTML = `
   <div class="card mx-auto m-3" style="width: 25rem;" >
-  <img class="card-img-top" src="${camera.imageUrl}">
-<div class="card-body d-flex flex-column ">
-    <h3 class="card-title mb-3">${camera.name}</h3>
-    <span class="fst-italic mb-2">${camera.description}</span>
-    <span class="fw-bold text-primary">${camera.price / 100}€</span>
-    <label for="lenses-select">Choisissez votre objectif:</label>
-    <select id="lenses">
-    <option value="">--Selectionnez votre objectif ici--</option>
-    ${camera.lenses.map((lense) => `<option>${lense}</option>`)}
-    </select>
-    <label for="tentacles">Quantités souhaiter:</label>
-    <input type="number" id="quantity" name="quantity"
-       min="0" max="50">
-       <input type="submit" class="btn btn-warning mt-2" value="Ajouter au panier">
-</div>
+    <img class="card-img-top" src="${camera.imageUrl}">
+    <div class="card-body d-flex flex-column ">
+      <h3 class="card-title mb-3">${camera.name}</h3>
+      <span class="fst-italic mb-2">${camera.description}</span>
+      <span class="fw-bold text-primary">${camera.price / 100}€</span>
+      <form>
+        <label for="lenses_option">Choisissez votre objectif:</label>
+        <select name="lenses_option" id="lenses_option">
+          <option value="">--Selectionnez votre objectif ici--</option>
+          ${camera.lenses.map((lense) => `<option>${lense}</option>`)}
+        </select>
+      </form>
+      <input type="submit" class="btn btn-warning mt-2" value="Ajouter au panier">
+    </div>
+  </div>
+
       `;
 };
 showCameraProduct(getCameraProduct());
+
+//---------------------------------------- GESTION DU PANIER -----------------------------------------------------------
+//-------- RECUPERATION DES DONNES SELECTIONNER  -----------------------------------------------------------------------
+// -- ID DU FORMULAIRE -------------------------------------------------------------------------------------------------
+
+// const
