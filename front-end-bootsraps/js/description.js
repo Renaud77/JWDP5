2bf1f11458f5e3ca0e638f4094f4e479abcb0868
Renaud <renaud_lefebvre@yahoo.fr>
2021-08-16 20:33:01 +0100

    setItem local storage

--- front-end-bootsraps/js/description.js
+++ front-end-bootsraps/js/description.js
@@ -15,29 +15,43 @@ const showCameraProduct = async (camera) => {
   const cameraProduct = document.querySelector("#product");
 
   cameraProduct.innerHTML = `
-  <div class="card mx-auto m-3" style="width: 25rem;" >
+  <div class="card mx-auto m-5" style="width: 25rem;" >
     <img class="card-img-top" src="${camera.imageUrl}">
     <div class="card-body d-flex flex-column ">
       <h3 class="card-title mb-3">${camera.name}</h3>
-      <span class="fst-italic mb-2">${camera.description}</span>
+      <span class="fst-italic mb-3">${camera.description}</span>
       <span class="fw-bold text-primary">${camera.price / 100}€</span>
-      <form>
+      <form id="lenses_option">
         <label for="lenses_option">Choisissez votre objectif:</label>
-        <select name="lenses_option" id="lenses_option">
-          <option value="">--Selectionnez votre objectif ici--</option>
-          ${camera.lenses.map((lense) => `<option>${lense}</option>`)}
-        </select>
+          <select name="lenses_option" id="lenses_option">
+            <option value="lenses">--Selectionnez votre objectif ici--</option>
+            ${camera.lenses.map(
+              (lense) => `<option id="lenses_option">${lense}</option>`
+            )}
+          </select>
+        <label for="quantity">Selectionner votre quantité :</label>
+          <input type="number" id="quantity" name="quantity" value="1" min="1" max="9" placeholder"1">
       </form>
-      <input type="submit" class="btn btn-warning mt-2" value="Ajouter au panier">
+      <input type="submit" onClick="addToBasket('${
+        camera._id
+      }' )" class="btn btn-warning mt-4" value="Ajouter au panier">
     </div>
   </div>
 
       `;
 };
+
 showCameraProduct(getCameraProduct());
 
-//---------------------------------------- GESTION DU PANIER -----------------------------------------------------------
-//-------- RECUPERATION DES DONNES SELECTIONNER  -----------------------------------------------------------------------
-// -- ID DU FORMULAIRE -------------------------------------------------------------------------------------------------
+const addQuantity = () => {
+  const ls = localStorage.getItem("basket");
+  const arrayBasket = ls.split(",");
+  console.log(arrayBasket);
+};
 
-// const
+function addToBasket(id) {
+  const quantity = document.querySelector("#quantity").value;
+  localStorage.setItem("basket", [id, quantity]);
+}
+
+addQuantity();
