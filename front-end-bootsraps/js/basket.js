b6bb1533933ad2d03b00076dccd10c5ba23a7b23
Renaud <renaud_lefebvre@yahoo.fr>
2021-08-27 20:54:23 +0100

    debut de la page panier

--- front-end-bootsraps/js/basket.js
+++ front-end-bootsraps/js/basket.js
@@ -1,8 +1,31 @@
-const main = document.querySelector(".main");
+// const main = document.querySelector(".main");
 
-if (getNumberOfItems() === 0) {
-  errorTittle = document.createElement("h2");
-  errorTittle.innerHTML = `Votre panier es vide.`;
-  errorMsg = document.createElement("p");
-  errorMsg.innerHTML = `Aucun produit n'as été selectionné dans votre panier.`;
+// if (getNumberOfItems() === 0) {
+//   errorTittle = document.createElement("h2");
+//   errorTittle.innerHTML = `Votre panier es vide.`;
+//   errorMsg = document.createElement("p");
+//   errorMsg.innerHTML = `Aucun produit n'as été selectionné dans votre panier.`;
+// }
+
+// on reutilise la const basket pour allez chercher nos info dans le localStorage
+
+const basket = JSON.parse(localStorage.getItem("basket"));
+console.log(basket);
+
+// on le affiche ensuite
+
+const displayItems = document.querySelector("#container_basket");
+console.log(displayItems);
+
+// message si le panier es vide
+
+if (basket === null) {
+  const emptyBasket = `    
+  <div class="empty_basket">
+    <h2 class="empty_basket-text">Votre panier es vide</h2>
+  </div>
+`;
+  basket.innerHTML = emptyBasket;
+} else {
+  console.log("je ne suis pas vide");
 }
